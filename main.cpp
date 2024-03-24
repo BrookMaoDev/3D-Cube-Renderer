3331967feeda90cd9f0e01a5a4f45ee6bac317a5
Brook Mao <brook.mao@mail.utoronto.ca>
2024-03-24 12:48:04 -0400

    Added dependencies

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
  * @details 3D Graphics Renderer
  */
 
+#include "include\SDL2\SDL.h"
+#include <vector>
 #include <iostream>
 using namespace std;
 
+#undef main
+
 int main()
 {
-    cout << "Heellloooo!!!!!";
+    cout << "Heellloooo!!!!!" << endl;
     return 0;
 }
